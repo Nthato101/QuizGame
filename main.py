06b238000ef26eb0f83a202a56157bde0b808f9c
Nthato <nthato101@gmail.com>
2023-08-14 18:50:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+# Create a question bank
+question_bank = []
+for item in question_data:
+    new_question = Question(item["text"], item["answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+
+while quiz.still_has_question():
+    quiz.next_question()
---
+++ question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, quest_text, quest_answer):
+        self.text = quest_text
+        self.answer = quest_answer
+
